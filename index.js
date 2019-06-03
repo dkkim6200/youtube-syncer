ef439d25a2f9cc40551705428b6ebd8bd6a09176
Daekun Kim <dkkim6200@gmail.com>
2019-06-02 20:08:36 -0700

    Basic socket communication works

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('index'));
+// app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('move to', (seconds) => {
+    console.log(`move to ${ seconds } seconds`);
+    io.emit('move to', seconds);
+  });
+
+  socket.on('play', () => {
+    console.log('play');
+    io.emit('play');
+  });
+
+  socket.on('pause', () => {
+    console.log('pause');
+    io.emit('pause');
+  });
+
+  socket.on('video cued', () => {
+    console.log('video cued');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on ${ PORT }`);
+});
